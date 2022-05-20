376826ba904d4e0b239f2e30704304dd1dd345d3
Oversoul <ttrungnguyenhp.2017@gmail.com>
2022-05-20 16:13:52 -0400

    Mobile Dev

---
+++ script.js
@@ -0,0 +1,13 @@
+
+document.getElementById("navicon").addEventListener("click", function(e){
+    var navbar = document.getElementById("menu");
+    navbar.style.display = "block";
+    navbar.style.transform += "translateX(-150px)";
+    document.addEventListener("click", exitListiner);
+})
+function exitListiner(evt){
+    if(evt.target.tagName != "UL" && evt.target.id != "navicon" && evt.target.tagName != "LI"){
+        document.getElementById("menu").style.transform += "translateX(150px)";
+        document.removeEventListener("click", exitListiner);
+    }
+}
