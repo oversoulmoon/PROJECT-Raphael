6c95969a91e2b17a3e1caae2804b1b2a4e46acb7
Oversoul <ttrungnguyenhp.2017@gmail.com>
2022-05-23 16:03:45 -0400

    ALMOST THERE T_T

--- script.js
+++ script.js
@@ -9,18 +9,9 @@ document.getElementById("upload").addEventListener("click", evt=>{
 
 document.getElementById("fileU").addEventListener("change", evt=>{
     var invalidFiles = 0; 
+    var fileIcon = document.createElement("img");
     Array.from(evt.target.files).forEach(file=>{
-        files.push(file);
-        var table = document.getElementById("fileList");
-        if(nextRow){
-            table.appendChild(document.createElement("tr"));
-            nextRow = false;
-        }else{
-            nextRow = true;
-        }
-        var tRows = table.rows;
-        var fileIcon = document.createElement("img");
-        fileIcon.setAttribute('class', 'fileImage');
+        var isInvalid = false; 
         switch(file.name.substring(file.name.lastIndexOf(".")+1)){
             case "txt":
                 fileIcon.setAttribute('src','resources/generalFile.png');
@@ -28,46 +19,72 @@ document.getElementById("fileU").addEventListener("change", evt=>{
             case "html":
                 fileIcon.setAttribute('src','resources/generalFile.png');
                 break;
-            case "docs":
+            case "css":
                 fileIcon.setAttribute('src','resources/generalFile.png');
                 break;
-            case "odf":
+            case "js":
                 fileIcon.setAttribute('src','resources/generalFile.png');
                 break;
             default:
                 invalidFiles++;
+                isInvalid = true; 
                 break;
         }
-        var fileSize = document.createElement("i");
-        if(file.size >= 1048678){
-            fileSize.innerText = (file.size/1048576) + " MBytes";
-        }else if(file.size >= 1024){
-            fileSize.innerText = (file.size/1048576) + " KBytes";
-        }else{
-            fileSize.innerText = (file.size) + " Bytes";
-
+        if(!isInvalid){
+            files.push(file);
+            var table = document.getElementById("fileList");
+            if(nextRow){
+                table.appendChild(document.createElement("tr"));
+                nextRow = false;
+            }else{
+                nextRow = true;
+            }
+            var tRows = table.rows;
+            fileIcon.setAttribute('class', 'fileImage');
+            var fileSize = document.createElement("i");
+            if(file.size >= 1048678){
+                var size = "" + (file.size/1048576);
+                size.substring(0,size.lastIndexOf('.')+2);
+                fileSize.innerText = size.substring(0,size.lastIndexOf('.')+2) + " MBytes";
+            }else if(file.size >= 1024){
+                var size = "" + (file.size/1024);
+                size.substring(0,size.lastIndexOf('.')+2);
+                fileSize.innerText = size.substring(0,size.lastIndexOf('.')+2) + " KBytes";
+            }else{
+                fileSize.innerText = (file.size) + " Bytes";
+            }
+            var fileName = document.createElement("b");
+            fileName.innerText = file.name;
+            var cell = document.createElement("td");
+            cell.setAttribute('class', 'file')
+            cell.append(fileIcon, fileName, document.createElement("br"),fileSize);
+            tRows[tRows.length - 1].appendChild(cell);
         }
-        var fileName = document.createElement("b");
-        fileName.innerText = file.name;
-        var cell = document.createElement("td");
-        cell.setAttribute('class', 'file')
-        cell.append(fileIcon, fileName, document.createElement("br"),fileSize);
-        tRows[tRows.length - 1].appendChild(cell);
-        //var tableCell = `<td class='file'><img class='fileImage' src='${fileIcon}'/><b>${fileName}</b><br/><i>${fileSize}</i></td>`;
-        //var fileRows = document.getElementById("fileList").rows;
-        //fileRows[fileRows.size-1].innerHTML += tableCell;
-    })
+    });
+    if(invalidFiles > 0){
+        window.alert(`Removed ${invalidFiles} invalid files from selection`);
+    }
 });
 document.getElementById("convertB").addEventListener("click", evt =>{
     files.forEach(file =>{
         var name = file.name.substring(0,file.name.lastIndexOf("."));
         var down = document.createElement("a");
-        down.setAttribute("download", `${name}.txt`);
+        var reader = new FileReader();
+        var preHtml = "<html xmlns:o='urn:schemas-microsoft-com:office:office' xmlns:w='urn:schemas-microsoft-com:office:word' xmlns='http://www.w3.org/TR/REC-html40'><head><meta charset='utf-8'><title>Export HTML To Doc</title></head><body><pre>";
+        var postHtml = "</pre></body></html>";
+    
+        down.setAttribute("download", `${name}.doc`);
         down.style.display = "none";
         down.id = "download" + file.name;
-        down.href = window.URL.createObjectURL(file);
-        document.getElementsByTagName("body")[0].appendChild(down);
-        document.getElementById("download" + file.name).click();
+        reader.readAsText(file);
+        reader.onload = () =>{
+            var content = reader.result;
+            var html = preHtml + content + postHtml;
+            down.href = 'data:application/vnd.ms-word;charset=utf-8,' + encodeURIComponent(html);
+            document.getElementsByTagName("body")[0].appendChild(down);
+            document.getElementById("download" + file.name).click();
+            document.getElementsByTagName("body")[0].removeChild(down);
+        }
     });
 });
 Array.from(document.getElementById("menu").children).forEach(element => {
